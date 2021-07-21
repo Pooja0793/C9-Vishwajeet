7f97370c05cfcecedda59b3991a3cf18bdc0bd3b
Pooja0793 <72892373+Pooja0793@users.noreply.github.com>
2021-07-21 15:01:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+   box = createSprite(200,200,50,50);
+}
+
+function draw() {
+  background("blue");
+
+  if(keyDown("right")){
+   box.x = box.x + 3;
+  }
+  
+  drawSprites();
+}
